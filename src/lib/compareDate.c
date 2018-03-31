51d69725e378715cf33cc5e75989c144a0525518
Pedro André Dias Barbosa <padbarbosa@gmail.com>
2018-03-31 16:27:25 +0100

    corrigi a funcao de comparar datas

--- src/lib/compareDate.c
+++ src/lib/compareDate.c
@@ -1,10 +1,12 @@
+#include <date.h>
+
 int isAfterBegin (Date begin, Date current){
-	anoBegin = get_year(begin);
-	anoCurrent = get_year(current);
-	mesBegin = get_month(begin);
-	mesCurrent = get_month(current);
-	diaBegin = get_day(begin);
-	diaCurrent = get_day(current);
+	int anoBegin = get_year(begin);
+	int anoCurrent = get_year(current);
+	int mesBegin = get_month(begin);
+	int mesCurrent = get_month(current);
+	int diaBegin = get_day(begin);
+	int diaCurrent = get_day(current);
 
 	if(anoCurrent >= anoBegin && mesCurrent >= mesBegin && diaCurrent >= diaBegin) return 1;
 	else return -1;
@@ -12,12 +14,12 @@ int isAfterBegin (Date begin, Date current){
 
 
 int isBeforeEnd (Date end, Date current){
-	anoEnd = get_year(end);
-	anoCurrent = get_year(current);
-	mesEnd = get_month(end);
-	mesCurrent = get_month(current);
-	diaEnd = get_day(end);
-	diaCurrent = get_day(current);
+	int anoEnd = get_year(end);
+	int anoCurrent = get_year(current);
+	int mesEnd = get_month(end);
+	int mesCurrent = get_month(current);
+	int diaEnd = get_day(end);
+	int diaCurrent = get_day(current);
 
 	if(anoCurrent <= anoEnd && mesCurrent <= mesEnd && diaCurrent <= diaEnd) return 1;
 	else return -1;
