88703ab9357e5e4f3d3174a0b54bfcef5439f5c0
a81317 <a81317@alunos.uminho.pt>
2018-03-31 18:52:37 +0100

    Adicionado GSlist com os posts por ordem cronologica(recente-antigo) e funçoes auxiliares

--- src/lib/interface.c
+++ src/lib/interface.c
@@ -4,10 +4,13 @@
 #include "/usr/include/libxml2/libxml/parser.h"
 #include "/usr/include/libxml2/libxml/xmlIO.h"
 #include <string.h>
+#include "date.h"
+
 
 struct TCD_community{
 	GHashTable *postshash;
 	GHashTable *usershash;
+	GSList *posts;
 };
 
 typedef struct post{
@@ -15,6 +18,7 @@ typedef struct post{
 	char *postTypeId;
 	char *ownerUserId;
 	int parentId;
+	Date creationDate;
 }*POST;
 
 typedef struct user_ht{
@@ -36,6 +40,7 @@ TAD_community init(){
 	t = malloc(sizeof(struct TCD_community));
 	t->postshash = g_hash_table_new (g_direct_hash, g_direct_equal);
 	t->usershash = g_hash_table_new (g_direct_hash, g_direct_equal);
+	t->posts = NULL;
 	return t;
 }
 
@@ -50,6 +55,55 @@ char* filePath(char *dump_path, char *file){
 	char *res = strcat(path, file);
 	return res;
 }
+
+/**
+* Função que converte a CreationDate no XML na struct date
+*/
+Date xmlCreationDate_to_Date(char* xmlDate) {
+	char s_year[2];
+	s_year[0] = xmlDate[2];
+	s_year[1] = xmlDate[3];
+	int year = 2000 + atoi(s_year);
+
+	char s_month[2];
+	s_month[0] = xmlDate[5];
+	s_month[1] = xmlDate[6];
+	int month = atoi(s_month);
+
+	char s_day[2];
+	s_day[0] = xmlDate[8];
+	s_day[1] = xmlDate[9];
+	int day = atoi(s_day);
+
+	Date d = createDate(day, month, year);
+	return d;
+}
+
+/*
+* Retorna 1 se a primeira data for a mais antiga, -1 se for a mais recente e 0 se forem iguais
+*/
+
+
+int compare_date (Date d1, Date d2){
+	int ano1 = get_year(d1);
+	int ano2 = get_year(d2);
+	int mes1= get_month(d1);
+	int mes2 = get_month(d2);
+	int dia1 = get_day(d1);
+	int dia2 = get_day(d2);
+
+	if(ano1 > ano2) return -1;
+	if(ano1 < ano2) return 1;
+
+	if(mes1 > mes2) return -1;
+	if(mes1 < mes2) return 1;
+
+	if(dia1 > dia2) return -1;
+	if(dia1 < dia2) return 1;
+
+	return 0;
+}
+
 
 /**
 * Função que faz load ao file Users.xml
@@ -128,10 +182,16 @@ void loadPosts(TAD_community com, char *dump_path, char *file){
 
 	ptr = cur->xmlChildrenNode;
 	ptr = ptr->next;
+
+
+
 	while(ptr != NULL){
 		POST p = malloc(sizeof(struct post));
 		key_posts = (char *) xmlGetProp(ptr, (xmlChar *) "Id");
 		int r_posts = atoi(key_posts);
+
+		p->creationDate = xmlCreationDate_to_Date((char*) xmlGetProp(ptr, (xmlChar *) "CreationDate"));
+
 		p->postTypeId =  (char *) xmlGetProp(ptr, (xmlChar *) "PostTypeId");
 
 		char *userId = (char *) xmlGetProp(ptr, (xmlChar *) "OwnerUserId");
@@ -149,10 +209,12 @@ void loadPosts(TAD_community com, char *dump_path, char *file){
 		else if(!strcmp(p->postTypeId, "2")){
 			p->parentId = atoi ((char *) xmlGetProp(ptr, (xmlChar *) "ParentId"));
 		}
+
+		com->posts = g_slist_prepend(com->posts, p);
+
 		g_hash_table_insert(com->postshash, GINT_TO_POINTER(r_posts), p);
 		ptr = ptr->next->next;
 	}
-	
 	xmlFreeDoc(doc);
 
 }
