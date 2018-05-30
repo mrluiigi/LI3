175122a795954c7fcfb2564537b29f50b3dc19b6
Luís Correia <luisbcorreia98@gmail.com>
2018-05-30 19:25:37 +0100

    Adicionada documentação ao ficheiro Users.java

--- proj-java/src/main/java/common/Users.java
+++ proj-java/src/main/java/common/Users.java
@@ -32,15 +32,24 @@ public class Users{
     return this.users.get(id);
   }
 
+  /**
+   * Insere um user
+   */
   public void add_myuser(int id, String name, String shortBio, int nr_posts, int lastPost, int reputation){
     MyUser u = new MyUser(id, name, shortBio, nr_posts, lastPost, reputation);
     this.users.put(id, u);
   }
 
+  /**
+   * Remove um user
+   */
   public void remove_myuser(int id){
     this.users.remove(id);
   }
 
+  /**
+   * Devolve uma lista com os N users que tem maior reputação
+   */
   public List<MyUser> get_N_users_with_most_reputation(int N) {
     Comparator c = (MyUser u1, MyUser u2) -> u2.getReputation() - u1.getReputation());
     Set<MyUser> set = sort_users(c);
@@ -55,6 +64,9 @@ public class Users{
     return res;
   }
 
+  /**
+   * Devolve uma lista com os users que tem o maior número de posts
+   */
   public List<MyUser> get_N_users_with_most_nr_posts(int N){
       Comparator c = (MyUser u1, MyUser u2) -> u2.getNr_posts() - u1.getNr_posts());
       List<MyUser> res = new ArrayList<>();
@@ -69,11 +81,17 @@ public class Users{
       return res;
   }
 
+  /**
+   * Incrementa o número de posts de um determinado user
+   */
   public void find_and_increment_user_nr_posts(int id) {
     	MyUSER user = this.users.get(id);
     	user.increment_user_nr_posts();
   }
 
+  /**
+   * Define o lastPost de uma dado user
+   */
   public void find_and_set_user_lastPost(int id, int lastPostId) {
   		MY_USER u = this.users.get(id);
   		if(u)
