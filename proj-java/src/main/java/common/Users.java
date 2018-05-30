e9ee7cae19cc66bdaa51314855b04fc82bab48ad
Luís Correia <luisbcorreia98@gmail.com>
2018-05-30 18:58:39 +0100

    Adicionada classe Users

---
+++ proj-java/src/main/java/common/Users.java
@@ -0,0 +1,83 @@
+import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class Users{
+
+  private Map<Integer, MyUser> users;
+
+  /**
+   * Construtor por omissão
+   */
+  public Users(){
+    this.users = new HashMap<>();
+  }
+
+  /**
+   * Método que ordena os users dado um comparador
+   */
+  public Set<MyUser> sort_users(Comparator<MyUser> c) {
+    Set<MyUser> res = new TreeSet(c);
+    for(int i : this.users.keySet()){
+      res.add(this.users.get(i).clone());
+    }
+    return res;
+  }
+
+  /**
+   * Encontra um determinado user dado o seu ID
+   */
+  public MyUser find_user(int id){
+    return this.users.get(id);
+  }
+
+  public void add_myuser(int id, String name, String shortBio, int nr_posts, int lastPost, int reputation){
+    MyUser u = new MyUser(id, name, shortBio, nr_posts, lastPost, reputation);
+    this.users.put(id, u);
+  }
+
+  public void remove_myuser(int id){
+    this.users.remove(id);
+  }
+
+  public List<MyUser> get_N_users_with_most_reputation(int N) {
+    Comparator c = (MyUser u1, MyUser u2) -> u2.getReputation() - u1.getReputation());
+    Set<MyUser> set = sort_users(c);
+    List<MyUser> res = new ArrayList<>();
+    int i = 0;
+    Iterator<MyUser> iterador = set.iterator();
+
+    while(i < N && iterador.hasNext()){
+      res.add(iterator.next());
+      i++;
+    }
+    return res;
+  }
+
+  public List<MyUser> get_N_users_with_most_nr_posts(int N){
+      Comparator c = (MyUser u1, MyUser u2) -> u2.getNr_posts() - u1.getNr_posts());
+      List<MyUser> res = new ArrayList<>();
+      Set<MyUser> set = sort_users_by_nr_posts(c);
+      int i = 0;
+      Iterator<MyUser> iterador = set.iterator();
+
+      while(i < N && iterator.hasNext()){
+        res.add(iterator.next());
+        i++;
+      }
+      return res;
+  }
+
+  public void find_and_increment_user_nr_posts(int id) {
+    	MyUSER user = this.users.get(id);
+    	user.increment_user_nr_posts();
+  }
+
+  public void find_and_set_user_lastPost(int id, int lastPostId) {
+  		MY_USER u = this.users.get(id);
+  		if(u)
+  			u.set_user_lastPost(lastPostId);
+  }
+
+}
