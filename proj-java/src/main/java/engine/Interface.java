6e6589e552be59c4deb2c8cb77bcc6a246d4f3d9
a81317 <a81317@alunos.uminho.pt>
2018-06-12 21:24:19 +0100

    Removidos includes desnecessários

--- proj-java/src/main/java/engine/Interface.java
+++ proj-java/src/main/java/engine/Interface.java
@@ -4,15 +4,8 @@ import common.MyLog;
 import common.Pair;
 
 import li3.TADCommunity;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -31,6 +24,7 @@ import java.util.Comparator;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
