8952269529d09e657360b8653c0a09fb5a5b573c
Luís Correia <luisbcorreia98@gmail.com>
2018-06-12 17:37:49 +0100

    Documentação TagInexistenteException

--- proj-java/src/main/java/engine/TagInexistenteException.java
+++ proj-java/src/main/java/engine/TagInexistenteException.java
@@ -1,8 +1,17 @@
 package engine;
 
-
+/**
+ * Classe que contém a excepção que ocorre quando se procura por uma tag inexistente.
+ *
+ * @author José Pinto (A81317); Luís Correia (A81141); Pedro "Bichão" Barbosa (A82068)
+ * @version 11/06/2018
+ */
 public class TagInexistenteException extends Exception
 {   
+	/**
+     * Excepção que ocorre quando se procura por uma Tag inexistente
+     * @param c Chave que levou a criar a excepçao
+     */
     public TagInexistenteException(String c){
         super(c);
     }
