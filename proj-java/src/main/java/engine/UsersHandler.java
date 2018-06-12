f4aaa4a156a5c8beb8b19477049d459c009dfdef
Luís Correia <luisbcorreia98@gmail.com>
2018-06-12 17:21:25 +0100

    Documentação TagsHandler

--- proj-java/src/main/java/engine/UsersHandler.java
+++ proj-java/src/main/java/engine/UsersHandler.java
@@ -7,18 +7,32 @@ import org.xml.sax.helpers.DefaultHandler;
 import java.time.LocalDate;
 import java.util.HashMap;
 
-
+/**
+ * Gera uma classe UsersHandler com toda a informação necessária  
+ * de todos os Users presentes no ficheiro Users.xml
+ *
+ * @author José Pinto (A81317); Luís Correia (A81141); Pedro Barbosa (A82068) // Grupo 26
+ * @version 11/06/2018
+ */
 public class UsersHandler extends DefaultHandler {
-
+   /** Utilizadores */
    public Users users;
 
 
 
-
+   /**
+    * Método que permite inicializar a classe UsersHandler
+    * (Construtor vazio)
+    */
    public UsersHandler() {
       this.users = new Users();
    }
 
+   /** 
+    * Método para devolver um LocalDate dado uma data em forma de String
+    * @param data Data em formato String
+    * @return LocalDate 
+    */
    public LocalDate createDate(String data){
        int ano = Integer.parseInt(data.substring(0, 4));
        int mes = Integer.parseInt(data.substring(5, 7));
@@ -27,9 +41,12 @@ public class UsersHandler extends DefaultHandler {
        return res;
    }
    
-
+   /**
+    * Carrega para a estrutura dos users todos os users presentes no ficheiro
+    */
    @Override
    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+      //Se a linha começar por "row"
       if (qName.equals("row")) {
         long id = Long.parseLong(attributes.getValue("Id"));
         String name = attributes.getValue("DisplayName");
@@ -37,6 +54,7 @@ public class UsersHandler extends DefaultHandler {
         int nr_posts = 0;
         long lastPost = 0;
         int reputation = Integer.parseInt(attributes.getValue("Reputation"));
+        //Adiciona esse User à variável dos Users
         users.add_myuser(id, name, shortBio, nr_posts, lastPost, reputation);
       }
    }
